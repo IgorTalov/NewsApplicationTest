9d523ed691ab405db8f6bd7155def6f5aed8fba1
Igor Talov <igortalov@gmail.com>
2016-06-29 19:03:43 +0300

    Initial Commit

---
+++ News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  News
+//
+//  Created by Игорь Талов on 29.06.16.
+//  Copyright © 2016 Игорь Талов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
